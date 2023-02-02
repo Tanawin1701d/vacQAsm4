6d3591c5707f4e9533187b6544f768ef028e6ac3
tanawin <umd-dev01-connect@outlook.com>
2023-02-02 18:48:10 +0700

    initialize git

---
+++ controllers/hospitals.js
@@ -0,0 +1,47 @@
+//@desc     get all hospitals
+//@route    GET /api/v1/hospitals
+//@access   Public
+
+exports.getHospitals=(req, res, next)=>{
+
+    res.status(200).json({success:true, msg:'Show all hospitals'});
+
+
+};
+
+//@desc     get single hospital
+//@route    GET /api/v1/hospitals/:id
+//@access   Public
+exports.getHospital=(req, res, next)=>{
+
+    res.status(200).json({success:true, msg:`Show hospital ${req.params.id}`});
+
+};
+
+//@desc     create new hospital
+//@route    POST /api/v1/hospitals
+//@access   Private
+exports.createHospital=(req, res, next)=>{
+
+    res.status(200).json({success:true, msg:'Create new hospitals'});
+
+};
+
+//@desc     update new hospital
+//@route    PUT /api/v1/hospitals
+//@access   Private
+exports.updateHospital=(req, res, next)=>{
+
+    res.status(200).json({success:true, msg:`Update hospital ${req.params.id}`});
+
+};
+
+
+//@desc     delete new hospital
+//@route    DELETE /api/v1/hospitals
+//@access   Private
+exports.deleteHospital=(req, res, next)=>{
+
+    res.status(200).json({success:true, msg:`Delete hospital ${req.params.id}`});
+
+};
